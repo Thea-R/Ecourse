478cf2d6eb61e940035be843b6cac4f9b10bcf78
tomato0228 <1273800239@qq.com>
2017-12-20 19:40:51 +0800

    增加哈希256加密函数

--- src/main/java/com/ecourse/untils/AESUtil.java
+++ src/main/java/com/ecourse/untils/AESUtil.java
@@ -6,6 +6,7 @@ import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -176,4 +177,28 @@ public class AESUtil {
         }
         return (base64Encode(md5(Keysb.toString()))).substring(0,16);
     }
+
+    /**
+     * 生成 HMACSHA256
+     * @param data 待处理数据
+     * @param key 密钥
+     * @return 加密结果
+     * @throws Exception 异常捕获
+     */
+    public static String HMACSHA256(String data, String key) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        byte[] array = sha256_HMAC.doFinal(data.getBytes("UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        for (byte item : array) {
+            sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
+        }
+        return sb.toString().toUpperCase();
+    }
+//
+//    public static void main(String[] args) throws Exception {
+//        System.out.println("admin的值是：---" + HMACSHA256("admin", "ecourse"));
+//        System.out.println("admin的值是：---" + HMACSHA256("admin", "ecourse"));
+//    }
 }
