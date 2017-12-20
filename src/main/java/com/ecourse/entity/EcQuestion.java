1cb00372d4d04ed0da6b1ffea87fb93cf84e1024
tomato0228 <1273800239@qq.com>
2017-12-20 19:38:50 +0800

    问题实体类修改，增加时间和课程

--- src/main/java/com/ecourse/entity/EcQuestion.java
+++ src/main/java/com/ecourse/entity/EcQuestion.java
@@ -1,18 +1,21 @@
 package com.ecourse.entity;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * @author tomato
- * @create 2017-12-18 下午4:59
+ * @create 2017-12-20 下午7:36
  */
 @Entity
 @Table(name = "ec_question", schema = "ecourse")
 public class EcQuestion {
     private int queId;
+    private Integer courseId;
     private Integer queType;
     private Integer queAns;
     private String queTitle;
+    private Date queTime;
 
     @Id
     @Column(name = "Que_Id", nullable = false)
@@ -22,6 +25,16 @@ public class EcQuestion {
 
     public void setQueId(int queId) {
         this.queId = queId;
+    }
+
+    @Basic
+    @Column(name = "Course_Id", nullable = true)
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
     }
 
     @Basic
@@ -54,6 +67,16 @@ public class EcQuestion {
         this.queTitle = queTitle;
     }
 
+    @Basic
+    @Column(name = "Que_Time", nullable = true)
+    public Date getQueTime() {
+        return queTime;
+    }
+
+    public void setQueTime(Date queTime) {
+        this.queTime = queTime;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -68,6 +91,9 @@ public class EcQuestion {
         if (queId != that.queId) {
             return false;
         }
+        if (courseId != null ? !courseId.equals(that.courseId) : that.courseId != null) {
+            return false;
+        }
         if (queType != null ? !queType.equals(that.queType) : that.queType != null) {
             return false;
         }
@@ -77,6 +103,9 @@ public class EcQuestion {
         if (queTitle != null ? !queTitle.equals(that.queTitle) : that.queTitle != null) {
             return false;
         }
+        if (queTime != null ? !queTime.equals(that.queTime) : that.queTime != null) {
+            return false;
+        }
 
         return true;
     }
@@ -84,9 +113,11 @@ public class EcQuestion {
     @Override
     public int hashCode() {
         int result = queId;
+        result = 31 * result + (courseId != null ? courseId.hashCode() : 0);
         result = 31 * result + (queType != null ? queType.hashCode() : 0);
         result = 31 * result + (queAns != null ? queAns.hashCode() : 0);
         result = 31 * result + (queTitle != null ? queTitle.hashCode() : 0);
+        result = 31 * result + (queTime != null ? queTime.hashCode() : 0);
         return result;
     }
 }
